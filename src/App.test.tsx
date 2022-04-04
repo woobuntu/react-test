b2db5f31b12a7f73292997b9f0fb3ead4f10695f
woobuntu <boutime2017@gmail.com>
2022-04-04 20:32:40 +0900

    App.test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,16 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders learn react link", () => {
   render(<App />);
+  // Create virtual DOM for argument JSX
+  // Access virtual DOM via screen global
+
   const linkElement = screen.getByText(/learn react/i);
+  // Find element by display text
+  // 여기서는 regular expression을 인자로 넘겼다.
+
   expect(linkElement).toBeInTheDocument();
+  // assertion, causes test to succeed or fail
 });
